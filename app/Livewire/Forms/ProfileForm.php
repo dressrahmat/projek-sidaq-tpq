8fb3c7bf08b5337dc62e7bec4482581fec12ef97
dressrahmat <dedysrahmat97@gmail.com>
2024-06-12 21:11:34 +0700

    profileform validasi image

--- app/Livewire/Forms/ProfileForm.php
+++ app/Livewire/Forms/ProfileForm.php
@@ -5,7 +5,6 @@ namespace App\Livewire\Forms;
 use Livewire\Form;
 use App\Models\Profile;
 use Livewire\Attributes\Rule;
-use Livewire\Attributes\Validate;
 use Illuminate\Support\Facades\Storage;
 
 class ProfileForm extends Form
@@ -35,8 +34,7 @@ class ProfileForm extends Form
     #[Rule('required', as: 'Alamat')]
     public $alamat;
 
-    #[Validate('max:2024', message: 'Anda melebihkan ukuran gambar', as: 'Photo Profile')]
-    #[Rule('required|image|mimes:jpg,jpeg,png|dimensions:width=300,height=400', as: 'Photo Profile')]
+    #[Rule('required|image|max:2024|mimes:jpg,jpeg,png|dimensions:width=300,height=400', as: 'Photo Profile')]
     public $photo_profile;
 
     public $admin;
